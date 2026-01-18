ce08fbc5ea4d6d2d9b347388881fe9ff50a4791d
brimason1316 <bri.mason1316@gmail.com>
2026-01-18 12:01:32 -0600

    Replace quote by Taylor Swift with a new quote

--- quotes.js
+++ quotes.js
@@ -1,7 +1,7 @@
 const QUOTES = [
   { text: "You do not yield.", author: "Sarah J. Maas" },
   { text: "Sometimes to self-discover, you must self-destruct.", author: "Robert M. Drake" },
-  { text: "Bah", author: "Taylor Swift" },
+  { text: "Think of your energy as if it is expensive. As if it is a luxury item.", author: "Taylor Swift" },
   { text: "Small steps still move you forward.", author: "Unknown" },
   { text: "If you want different results, do different reps.", author: "Unknown" }
 ];
