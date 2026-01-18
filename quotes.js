28aa6db35b30e53153cba1639af73b09558a1b54
brimason1316 <bri.mason1316@gmail.com>
2026-01-18 11:55:05 -0600

    Update quotes with new entries and authors

--- quotes.js
+++ quotes.js
@@ -1,7 +1,9 @@
 const QUOTES = [
-  { text: "You do not yield.", author: "Unknown" },
+  { text: "You do not yield.", author: "Sarah J. Maas" },
   { text: "Sometimes to self-discover, you must self-destruct.", author: "Robert M. Drake" },
-  { text: "You don’t have to feel ready. You have to start.", author: "Unknown" },
+  { text: "Think of **your** **energy** as if it’s expensive. As if it’s a **luxury** **item**. 
+
+Not everyone (or everything) can **afford** it.", author: "Taylor Swift" },
   { text: "Small steps still move you forward.", author: "Unknown" },
   { text: "If you want different results, do different reps.", author: "Unknown" }
 ];
