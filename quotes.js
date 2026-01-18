34a136558c911a8d7229a4478e8e5e44413779e8
brimason1316 <bri.mason1316@gmail.com>
2026-01-18 11:51:33 -0600

    Update quotes in quotes.js
    Removed two quotes and added two new quotes to the list.

--- quotes.js
+++ quotes.js
@@ -1,6 +1,6 @@
 const QUOTES = [
-  { text: "Do the thing you keep avoiding.", author: "Unknown" },
-  { text: "Action is the antidote to anxiety.", author: "Unknown" },
+  { text: "You do not yield.", author: "Unknown" },
+  { text: "Sometimes to self-discover, you must self-destruct.", author: "Robert M. Drake" },
   { text: "You don’t have to feel ready. You have to start.", author: "Unknown" },
   { text: "Small steps still move you forward.", author: "Unknown" },
   { text: "If you want different results, do different reps.", author: "Unknown" }
